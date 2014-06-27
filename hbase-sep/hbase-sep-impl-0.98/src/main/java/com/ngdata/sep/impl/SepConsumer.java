09d31ef51c1f6e8f72cf694db485fb99bd15ce5a
Jan Van Besien <janvanbesien@ngdata.com>
2014-06-27 15:05:23 +0200

    fix thread resource leak

--- hbase-sep/hbase-sep-impl-0.98/src/main/java/com/ngdata/sep/impl/SepConsumer.java
+++ hbase-sep/hbase-sep-impl-0.98/src/main/java/com/ngdata/sep/impl/SepConsumer.java
@@ -208,6 +208,9 @@ public class SepConsumer extends BaseHRegionServer {
             }
         }
         sepMetrics.shutdown();
+        for (ThreadPoolExecutor executor : executors) {
+            executor.shutdown();
+        }
     }
 
     public boolean isRunning() {

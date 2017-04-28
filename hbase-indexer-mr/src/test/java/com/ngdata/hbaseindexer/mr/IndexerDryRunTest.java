34af8db9bd9a24c5ef35eadb3c820db8697baca9
Wolfgang Hoschek <whoschek@cloudera.com>
2017-04-27 19:00:49 -0700

    HIX-84: Update to hadoop3-alpha2, part3: reenable InderDryRunTest

--- hbase-indexer-mr/src/test/java/com/ngdata/hbaseindexer/mr/IndexerDryRunTest.java
+++ hbase-indexer-mr/src/test/java/com/ngdata/hbaseindexer/mr/IndexerDryRunTest.java
@@ -37,7 +37,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -48,7 +47,6 @@ import java.util.Map.Entry;
 
 import static org.junit.Assert.assertEquals;
 
-@Ignore
 public class IndexerDryRunTest {
 
     private static final byte[] TEST_TABLE_NAME = Bytes.toBytes("record");

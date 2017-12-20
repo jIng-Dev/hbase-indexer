819a6ff3b5fd2aad3c2bd599a800157246351c7f
Carl Myers <cmyers@cloudera.com>
2017-12-20 15:48:50 -0800

    CDH-57544: update precommit build to use java8
    This is part of the c6 move to precommit workflow
    
    Change-Id: I26bb0c29e45a79a7b9704704ac3790ae8cb5918b

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -1,6 +1,4 @@
+#!/usr/bin/env bash
 # CLOUDERA-BUILD
-export JAVA7_BUILD=true
 export MAVEN_OPTS=${MAVEN_OPTS} -Xmx768m -XX:MaxPermSize=128m
-. /opt/toolchain/toolchain.sh
-
 mvn clean compile test-compile

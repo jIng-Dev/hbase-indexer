845f25bd4fbdbc878b613d0f7721cbd2cdfbee93
Wolfgang Hoschek <whoschek@cloudera.com>
2018-03-20 10:50:43 -0700

    CDH-66250: precommit hook script for search and hbase-indexer should also run unit tests
    Change-Id: Ia31c1d04b1eabf0e360ce86f2477230bbec71433

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -7,4 +7,4 @@ export MAVEN_OPTS="${MAVEN_OPTS} -Xmx768m -XX:MaxPermSize=128m"
 # activate mvn-gbn wrapper
 mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
 
-mvn -U clean compile test-compile
+mvn --update-snapshots --batch-mode -Dmaven.test.failure.ignore=true -Dtests.nightly=true --fail-at-end clean install
